284794f2ad5ff71436eb2cd496377c8bd649aa93
dhktjr0204 <dhktjr0204442@gmail.com>
2024-02-20 16:46:03 +0900

    2월 20일 데일리 퀴즈

---
+++ dailyQuiz/datily0220/quiz1.java
@@ -0,0 +1,33 @@
+package datily0220;
+
+import java.io.*;
+import java.util.*;
+
+public class quiz1 {
+    public List<Integer> solution(int n, int[] array) {
+        List<Integer> answer = new ArrayList<>();
+        answer.add(array[0]);
+        for(int i=1;i<n;i++){
+            if(array[i]>array[i-1]){
+                answer.add(array[i]);
+            }
+        }
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        quiz1 quiz1 = new quiz1();
+        Scanner scanner = new Scanner(System.in);
+
+        int number = scanner.nextInt();
+        int[] array = new int[number];
+
+        for (int i = 0; i < number; i++) {
+            array[i] = scanner.nextInt();
+        }
+
+        for (int x : quiz1.solution(number, array)) {
+            System.out.print(x + " ");
+        }
+    }
+}
---
+++ dailyQuiz/datily0220/quiz2.java
@@ -0,0 +1,42 @@
+package datily0220;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class quiz2 {
+    public int[] solution(int[] array1, int[] array2) {
+        int[] result = new int[array1.length + array2.length];
+        // 코드 작성
+        for (int i = 0; i < array1.length; i++) {
+            result[i]=array1[i];
+        }
+        for (int i = 0; i < array2.length; i++) {
+            result[i+array1.length]=array2[i];
+        }
+        Arrays.sort(result);
+        return result;
+    }
+
+    public static void main(String[] args) {
+        quiz2 ascendingSort = new quiz2();
+        Scanner scanner = new Scanner(System.in);
+
+        int number = scanner.nextInt();
+        int[] array = new int[number];
+
+        for (int i = 0; i < number; i++) {
+            array[i] = scanner.nextInt();
+        }
+
+        int number2 = scanner.nextInt();
+        int[] array2 = new int[number2];
+
+        for (int i = 0; i < number2; i++) {
+            array2[i] = scanner.nextInt();
+        }
+
+        for (int x : ascendingSort.solution(array, array2)) {
+            System.out.print(x + " ");
+        }
+    }
+}
